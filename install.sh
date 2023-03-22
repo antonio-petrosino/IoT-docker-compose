b13ff0001e7ecdcde4e55714b5d8685e2eeb00b8
antonio-petrosino <72913542+antonio-petrosino@users.noreply.github.com>
2023-03-22 18:39:07 +0100

    new dashboards

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@ sudo service docker start
 sudo docker-compose up -d
 docker exec -it influxdb influx -execute "CREATE DATABASE iotlab"
 docker exec -it nodered npm install --prefix /data node-red-contrib-influxdb
+docker exec grafana grafana-cli plugins install agenty-flowcharting-panel
 docker-compose restart nodered
+docker-compose restart influxdb
+docker-compose restart grafana
